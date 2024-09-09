78ace204d107fb23f597c00fc4c23cd63b6d1003
jhonas8 <love.android62@gmail.com>
2024-09-09 12:40:37 -0300

    [improvement]: improves the logic to populate the makes collection

--- src/migration.ts
+++ src/migration.ts
@@ -2,14 +2,26 @@ import { connectToDatabase } from "./database/connection";
 import { fetchDataRoutine } from "./routines/fetchData.routine";
 import { populateMongodbRoutine } from "./routines/populateMongodb.routine";
 import { logAction } from "./utils/logAction";
+import { Makes } from "./database/models";
 
-const seedDatabase = async () => {
-    await logAction('Connecting to the database', connectToDatabase)
-
-    const data = await logAction('fetching data', fetchDataRoutine);
-
-    await logAction('Populating mongo database with the fetched data', async () => await populateMongodbRoutine(data));
-
+const fetchMakes = async () => {
+    // Fetch all documents from the 'makes' collection
+    const makes = await logAction('Fetching makes from the database', async () => await Makes.find({}));
+    return makes;
 };
 
-seedDatabase();
+const seedDatabase = async () => {
+    await logAction('Connecting to the database', connectToDatabase);
+
+    const data = await logAction('fetching data', fetchDataRoutine);
+    const makes = await fetchMakes();
+
+    // Check if the size of 'makes' is the same as 'data'
+    if (makes.length === data.length)
+        return console.log('Skipping population: makes collection already has the same amount of data.');
+
+    await logAction('Populating mongo database with the fetched data', async () => await populateMongodbRoutine(data));
+};
+
+
+seedDatabase()
